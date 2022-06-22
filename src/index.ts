4ab4397f65e7ebe0b8d6f5749371ffe3ff9a19b9
eblocha <eblocha@protonmail.com>
2022-06-22 06:51:38 -0400

    export withPermissions from index

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { KeyedMutex } from "./keyed";
 export { RWMutex } from "./readwrite";
 export { ReentrantMutex, IDomain } from "./reentrant";
 export { ILock, Releaser } from "./interfaces";
+export { withPermissions } from "./utils";
 export { Mutex } from "async-mutex";
